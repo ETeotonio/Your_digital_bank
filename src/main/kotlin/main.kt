5d3b37a815346512e1e156a5d0419fe4bc8bc4b9
Eliel <eliel.teotonio@gmail.com>
2022-05-19 20:41:46 +0100

    2nd lesson

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,24 @@
 fun main(){
-    val titular = "Carlos"
-    if(!(titular.isBlank())){
-        println("Hello world $titular")
+    var titular:String = "Carlos"
+    val numAccount = 100200300
+    var amount:Double = 0.00
+
+//    amount=(100.00+2)
+//    amount+=200
+
+    amount=0.00
+
+    println("Titular: $titular")
+    println("Account Number: $numAccount")
+    checkValue(amount)
+
+
+}
+
+fun checkValue(amount:Double){
+    when{
+        amount > 0.00->println("Negative value")
+        amount == 0.00 -> println("Empty acct")
+        else -> println("Positive")
     }
-
-
 }
